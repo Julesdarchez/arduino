1bcf2c98c4c09c80cf38ecc2bca664cec8ce9307
Julesdarchez <jules.darchez@gmail.com>
2018-04-09 17:29:36 +0200

    Interro fini avec brio

--- main.ino
+++ main.ino
@@ -1,14 +1,11 @@
+#include "Arduino.h"
+
 void setup()
 {
-    pinMode(13, OUTPUT);
 }
 
 void loop()
 {
     static int potPosition;
-    potPosition = analogRead(A0);
-    digitalWrite(13, HIGH);
-    delay(2000)
-    digitalWrite(13, LOW);
-    delay(2000)
+    potPosition = analogRead(A2);
 }
